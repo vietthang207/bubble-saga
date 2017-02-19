22698ee3991a2930033a15196441a67ac1d85c7d
dinhvt <dinhvt@soctf-imac-014.comp.nus.edu.sg>
2017-02-19 21:05:35 +0800

    create project

---
+++ BubbleSaga/BubbleSaga/GameMenuViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BubbleSaga
+//
+//  Created by dinhvt on 19/2/17.
+//  Copyright © 2017 nus.cs3217.a0126513. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ BubbleSaga/BubbleSaga/LevelDesignerViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LevelDesignerViewController.swift
+//  BubbleSaga
+//
+//  Created by dinhvt on 19/2/17.
+//  Copyright © 2017 nus.cs3217.a0126513. All rights reserved.
+//
+
+import Foundation
